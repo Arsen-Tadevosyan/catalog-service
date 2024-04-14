9cb10f86cc2077417674b51273020d624cf6ddee
arsen tadevosyan <arsentadevosyan370@gmail.com>
2024-04-15 01:40:17 +0400

    catalog-service

--- src/main/java/com/example/catalogservice/HomeController.java
+++ src/main/java/com/example/catalogservice/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String getGreeting(){
-        return "Welcome to the bookshop catalog";
+        return "Welcome to the bookshop catalog ";
     }
 
 }
